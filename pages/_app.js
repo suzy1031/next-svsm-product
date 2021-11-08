b0fed7bca3f960e46758673aae0b0847c5c38a26
kohei-sugawara <89954502+kohei-sugawara@users.noreply.github.com>
2021-11-08 15:15:53 +0900

    material ui config

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,28 @@
+import React from "react";
+
 import App from "next/app";
 import "../styles/globals.css";
 
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../src/theme";
+
 import basicAuthCheck from "../src/basicAuthCheck";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  React.useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
 MyApp.getInitialProps = async (appContext) => {
