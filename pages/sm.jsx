fac3eabeb129335d937735b97f3795fed688a13e
kohei-sugawara <89954502+kohei-sugawara@users.noreply.github.com>
2021-11-11 23:07:54 +0900

    modify sv / sm component

--- pages/sm.jsx
+++ pages/sm.jsx
@@ -1,8 +1,38 @@
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
+import Switch from "@material-ui/core/Switch";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
+import {
+  storeManager,
+  storeSubManager,
+  department,
+  allStoreAverage,
+  initData,
+} from "../src/services/data";
+
+import switchRole from "../src/libs/SwitchRole";
+import convertSkillName from "../src/libs/ConvertSkillName";
+import { levelColor } from "../src/libs/LevelColor";
+import useCalculation from "../src/application/CalcStoreAverage";
+import useGenerateMonthObject from "../src/application/GrowLawn";
 
 import Header from "../src/ui/Common/Header";
 import SideMenuDrawer from "../src/ui/Common/SideMenuDrawer";
 import PageTitle from "../src/ui/Common/PageTitle";
+import StoreModal from "../src/ui/Sm/StoreModal";
+import StaffModal from "../src/ui/Sm/StaffModal";
+import SkillsRadarChart from "../src/ui/Sm/SkillsRadarChart";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,10 +42,181 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
   },
+  mainColor: {
+    background: "#17b397",
+    "&:hover": {
+      background: "#008080",
+    },
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    marginLeft: 300,
+  },
+
+  cardRoot: {
+    minWidth: 275,
+    maxWidth: 330,
+    border: "1px solid black",
+    margin: 10,
+  },
+  title: {
+    fontSize: 14,
+  },
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
 }));
 
-export default function Report() {
+export default function Sm() {
   const classes = useStyles();
+
+  const [open, setOpen] = useState(false);
+  const [type, setType] = useState({
+    type: "",
+    name: "",
+  });
+
+  const [selected, setSelected] = useState("");
+
+  // モーダル表示切替え
+  const handleOpen = (buttonType) => {
+    setSelected(buttonType);
+    if (buttonType === "departmentName") {
+      setType({
+        attribute: buttonType,
+        name: "部門",
+      });
+    } else {
+      setType({
+        attribute: "staffName",
+        name: "メンバー",
+      });
+    }
+    setOpen(true);
+  };
+
+  // モーダル閉じる
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const [value, setValue] = useState({});
+
+  const handleInput = (e) => {
+    setValue({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const [data, setData] = useState(department);
+
+  // 部門追加
+  const submit = () => {
+    const pushData = {
+      ...value,
+      members: [],
+    };
+    department.push(pushData);
+    setOpen(false);
+  };
+
+  // メンバー追加
+  const memberAddSubmit = () => {
+    const inputMember = {
+      ...value,
+      image: "/images/user.jpeg",
+      role: 4,
+      skills: [
+        { skillName: "hr", level: 50 },
+        { skillName: "vmd", level: 50 },
+        { skillName: "cs", level: 50 },
+        { skillName: "stock", level: 50 },
+        { skillName: "pc", level: 50 },
+      ],
+    };
+    for (let i = 0; i < department.length; i++) {
+      if (selected === department[i].departmentName) {
+        department[i].members.push(inputMember);
+      }
+    }
+    setOpen(false);
+  };
+
+  const [staffModalOpen, setStaffModalOpen] = useState(false);
+  const [staffData, setStaffData] = useState(null);
+  const [smData, setSmData] = useState(storeManager);
+  const [ssmData, setSsmData] = useState(storeSubManager);
+
+  // メンバーモーダル開く
+  const handleStaffDetail = (profile) => {
+    setStaffData(profile);
+    setStaffModalOpen(true);
+  };
+
+  // メンバーモーダル閉じる
+  const handleStaffModalClose = () => {
+    setStaffModalOpen(false);
+  };
+
+  const [chartData, setChartData] = useState(initData);
+
+  // レーダーチャートデータオブジェクト
+  const { hr, vmd, cs, stock, pc } = useCalculation(
+    smData,
+    ssmData,
+    department
+  );
+
+  useEffect(() => {
+    setChartData({
+      sm: smData.smSkills.level,
+      ssm: ssmData.smSkills.level,
+      hr: hr,
+      vmd: vmd,
+      cs: cs,
+      stock: stock,
+      pc: pc,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const [checked, setChecked] = useState(false);
+
+  // 全店レーダーチャート表示
+  const toggleChecked = () => {
+    setChecked((prev) => !prev);
+  };
+
+  // TalkCard
+  const [isNext, setIsNext] = useState(false);
+  const [isDefault, setIsDefault] = useState(true);
+  const handleFinish = () => {
+    setIsDefault(!isDefault);
+    setIsNext(!isNext);
+  };
+
+  const [radioChecked, setRadioChecked] = useState("9");
+
+  const [membersName, setMembersName] = useState([]);
+  const [checkedMember, setCheckedMember] = useState({});
+  useEffect(() => {
+    const members = data.map((department) =>
+      department.members.map((staffs) => staffs.staffName)
+    );
+    let membersArray = [];
+    for (let i = 0; i < members.length; i++) {
+      membersArray.push(...members[i]);
+    }
+    setMembersName(membersArray);
+  }, []);
+
+  const { days, month, thisMonthData, setThisMonthData } =
+    useGenerateMonthObject();
 
   return (
     <>
@@ -23,8 +224,163 @@ export default function Report() {
       <Header headerTitle="組織図" />
       <SideMenuDrawer />
       <main className={classes.content}>
-        <h1 style={{ color: "black", margin: "60px 0 0 300px" }}>組織図 </h1>
+        <div className={classes.toolbar} />
+        <Grid container>
+          <Grid item xs={6}>
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Button
+                variant="contained"
+                className={classes.mainColor}
+                onClick={() => handleStaffDetail(smData)}
+              >
+                <Avatar alt={smData.staffName} src={smData.image} />
+                <Box p={1} style={{ color: "#fff" }}>
+                  {smData.staffName}
+                </Box>
+              </Button>
+              <div
+                style={{ height: "20px", border: "1px solid #c0c0c0" }}
+              ></div>
+              <Button
+                variant="contained"
+                className={classes.mainColor}
+                onClick={() => handleStaffDetail(ssmData)}
+              >
+                <Avatar alt={ssmData.staffName} src={ssmData.image} />
+                <Box p={1} style={{ color: "#fff" }}>
+                  {ssmData.staffName}
+                </Box>
+              </Button>
+              <div
+                style={{ height: "20px", border: "1px solid #c0c0c0" }}
+              ></div>
+            </Grid>
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="flex-start"
+              style={{ border: "1px solid #c0c0c0", position: "relative" }}
+            >
+              {data.map((item, index) => (
+                <Card
+                  key={index}
+                  className={classes.cardRoot}
+                  variant="outlined"
+                >
+                  <CardContent>
+                    <Typography className={classes.title}>
+                      <a>詳しく見る</a>
+                    </Typography>
+                    <Box display="flex">
+                      <Box p={1} flexGrow={1}>
+                        <Typography variant="h5" component="h2">
+                          {item.departmentName}
+                        </Typography>
+                      </Box>
+                      <Box p={1}>
+                        <Button
+                          className={classes.mainColor}
+                          style={{ color: "#fff", fontSize: 8 }}
+                          onClick={() => handleOpen(item.departmentName)}
+                        >
+                          メンバー追加
+                        </Button>
+                      </Box>
+                    </Box>
+                  </CardContent>
+                  <CardActions>
+                    <Box display="flex" flexWrap="wrap" sx={{ minWidth: 335 }}>
+                      {item.members.length >= 1 ? (
+                        item.members.map((profile, index) => (
+                          <Button
+                            size="small"
+                            key={index}
+                            variant="outlined"
+                            onClick={() => handleStaffDetail(profile)}
+                          >
+                            <Avatar
+                              alt={profile.staffName}
+                              src={profile.image}
+                              className={classes.small}
+                            />
+                            {profile.staffName}
+                          </Button>
+                        ))
+                      ) : (
+                        <>
+                          <ErrorOutlineIcon />
+                          メンバーがまだ登録されていません
+                        </>
+                      )}
+                    </Box>
+                  </CardActions>
+                </Card>
+              ))}
+              <Button
+                className={classes.mainColor}
+                style={{
+                  position: "absolute",
+                  bottom: 5,
+                  right: 5,
+                  color: "#fff",
+                }}
+                onClick={() => handleOpen("departmentName")}
+              >
+                部門を追加
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid item xs={6}>
+            <Grid
+              container
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="center"
+            >
+              <h2 style={{ marginLeft: 40, marginRight: 10 }}>店舗レベル</h2>
+              <FormGroup>
+                <FormControlLabel
+                  control={
+                    <Switch
+                      checked={checked}
+                      onChange={toggleChecked}
+                      color="primary"
+                    />
+                  }
+                  label={checked ? "ON（全店と比較）" : "OFF（全店と比較）"}
+                />
+              </FormGroup>
+            </Grid>
+            <SkillsRadarChart
+              chartData={chartData}
+              checked={checked}
+              allStoreAverage={allStoreAverage}
+            />
+          </Grid>
+        </Grid>
       </main>
+      <StoreModal
+        open={open}
+        handleClose={handleClose}
+        handleInput={handleInput}
+        submit={submit}
+        memberAddSubmit={memberAddSubmit}
+        type={type}
+      />
+      <StaffModal
+        open={staffModalOpen}
+        handleClose={handleStaffModalClose}
+        profile={staffData}
+        switchRole={switchRole}
+        convertSkillName={convertSkillName}
+        levelColor={levelColor}
+      />
     </>
   );
 }
--- pages/sv.jsx
+++ pages/sv.jsx
@@ -1,8 +1,31 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+
 import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
+import { stores, initData, area, otherArea } from "../src/services/data";
+import switchRole from "../src/libs/SwitchRole";
 
 import Header from "../src/ui/Common/Header";
 import SideMenuDrawer from "../src/ui/Common/SideMenuDrawer";
 import PageTitle from "../src/ui/Common/PageTitle";
+import StoreRadarChart from "../src/ui/Sv/StoreRadarChart";
+import CheckButtonGroup from "../src/ui/Sv/CheckButtonGroup";
+import AllAreaOrganization from "../src/ui/Sv/AllAreaOrganization";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,10 +35,107 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
   },
+  mainColor: {
+    background: "#17b397",
+    "&:hover": {
+      background: "#008080",
+    },
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+
+  content: {
+    flexGrow: 1,
+    paddingTop: theme.spacing(3),
+    marginLeft: 300,
+  },
+
+  formControl: {
+    margin: theme.spacing(3),
+  },
+  title: {
+    fontSize: 14,
+  },
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+  tagCircle: {
+    height: 20,
+    width: 20,
+    borderRadius: "50%",
+    margin: "16px 5px",
+  },
+  levelButton: {
+    display: "inline-block",
+    width: "20px",
+    height: "20px",
+    borderRadius: "50%",
+    textAlign: "center",
+    lineHeight: "20px",
+    marginLeft: 3,
+    color: "#FFF",
+  },
+  selectFormControl: {
+    margin: theme.spacing(1),
+    width: 200,
+  },
 }));
 
 export default function Report() {
   const classes = useStyles();
+
+  const [checked, setChecked] = useState({
+    id_0: true,
+    id_1: true,
+    id_2: true,
+  });
+  const [areaStoreData, setAreaStoreData] = useState(stores);
+
+  const handleChange = (event) => {
+    setChecked({ ...checked, [event.target.id]: event.target.checked });
+  };
+
+  const [chartData, setChartData] = useState(initData);
+  const [chartDataB, setChartDataB] = useState(initData);
+  const [chartDataC, setChartDataC] = useState(initData);
+  useEffect(() => {
+    areaStoreData.map((store) => {
+      if (store.storeName === "渋谷店") {
+        setChartData(store.skills);
+      }
+      if (store.storeName === "原宿店") {
+        setChartDataB(store.skills);
+      }
+      if (store.storeName === "新宿店") {
+        setChartDataC(store.skills);
+      }
+      return {};
+    });
+  }, []);
+
+  const [radioChecked, setRadioChecked] = useState("myArea");
+  const handleAreaChange = (event) => {
+    setRadioChecked(event.target.value);
+  };
+
+  const [myAreaData, setMyAreaData] = useState(area);
+  const [otherAreaData, setOtherAreaData] = useState(otherArea);
+  const [selected, setSelected] = useState("");
+  const [targetData, setTargetData] = useState(null);
+  const handleSelectChange = (event) => {
+    setSelected(event.target.value);
+    const selectedValue = event.target.value;
+
+    if (selectedValue) {
+      otherAreaData.map((store) => {
+        if (store.areaName === selectedValue) {
+          setTargetData(store);
+        }
+        return {};
+      });
+    }
+  };
 
   return (
     <>
@@ -23,9 +143,160 @@ export default function Report() {
       <Header headerTitle="組織図（SV）" />
       <SideMenuDrawer />
       <main className={classes.content}>
-        <h1 style={{ color: "black", margin: "60px 0 0 300px" }}>
-          組織図（SV）
-        </h1>
+        <div className={classes.toolbar} />
+        <Grid container>
+          <Grid
+            container
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            style={{ width: 400 }}
+          >
+            <Grid>
+              <FormLabel component="legend">エリア選択</FormLabel>
+              <RadioGroup
+                aria-label="gender"
+                name="gender1"
+                value={radioChecked}
+                onChange={handleAreaChange}
+                row
+              >
+                <FormControlLabel
+                  value="myArea"
+                  control={<Radio />}
+                  label="自エリア"
+                />
+                <FormControlLabel
+                  value="allArea"
+                  control={<Radio />}
+                  label="全エリア"
+                />
+              </RadioGroup>
+            </Grid>
+            {radioChecked === "myArea" ? (
+              <CheckButtonGroup
+                labelName="店舗選択"
+                data={areaStoreData}
+                handleChange={handleChange}
+                checked={checked}
+              />
+            ) : (
+              <Grid
+                container
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="flex-start"
+              >
+                <FormControl
+                  variant="outlined"
+                  className={classes.selectFormControl}
+                >
+                  <InputLabel id="demo-simple-select-outlined-label">
+                    比較エリア
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-outlined-label"
+                    id="demo-simple-select-outlined"
+                    value={selected}
+                    onChange={handleSelectChange}
+                    label="比較エリア"
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    {otherArea.map((store, index) => (
+                      <MenuItem key={index} value={store.areaName}>
+                        {store.areaName}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
+            )}
+          </Grid>
+        </Grid>
+        {radioChecked === "myArea" ? (
+          <Grid container>
+            <Grid item xs={5}>
+              {areaStoreData.map((store, index) => (
+                <React.Fragment key={index}>
+                  {checked[`id_${index}`] && (
+                    <React.Fragment>
+                      <Box display="flex">
+                        <div
+                          className={classes.tagCircle}
+                          style={store.style}
+                        ></div>
+                        <Link href="/sm">
+                          <a>
+                            <h3 id={`id_${index}`}>{store.storeName}</h3>
+                          </a>
+                        </Link>
+                      </Box>
+                      <Card
+                        key={index}
+                        className={classes.cardRoot}
+                        variant="outlined"
+                      >
+                        <CardContent>
+                          <Box
+                            display="flex"
+                            flexWrap="wrap"
+                            sx={{ minWidth: 335 }}
+                          >
+                            {store.members.map((profile, index) => (
+                              <Box mr={2} key={index}>
+                                <div>{switchRole(profile.role)}</div>
+                                <Button
+                                  size="small"
+                                  key={index}
+                                  variant="outlined"
+                                  onClick={() => console.log("click")}
+                                >
+                                  <Avatar
+                                    alt={profile.staffName}
+                                    src={profile.image}
+                                    className={classes.small}
+                                  />
+                                  {profile.staffName}
+                                </Button>
+                              </Box>
+                            ))}
+                          </Box>
+                        </CardContent>
+                      </Card>
+                    </React.Fragment>
+                  )}
+                </React.Fragment>
+              ))}
+            </Grid>
+            <Grid item xs={6}>
+              <Grid
+                container
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="center"
+              >
+                <StoreRadarChart
+                  chartData={chartData}
+                  chartDataB={chartDataB}
+                  chartDataC={chartDataC}
+                  checked={checked}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+        ) : (
+          <AllAreaOrganization
+            data={myAreaData}
+            otherArea={otherAreaData}
+            classes={classes}
+            checked={checked}
+            selected={selected}
+            handleSelectChange={handleSelectChange}
+            targetData={targetData}
+          />
+        )}
       </main>
     </>
   );
